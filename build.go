fe897bcc1298c7c10f4a7787cf3404d579bdba30
Jonas Schneider <mail@jonasschneider.com>
2015-10-05 18:49:01 +0200

    sigh

--- build.go
+++ build.go
@@ -29,7 +29,7 @@ func fetchScript(github_repo_name, rev string) (error, io.Reader) {
   req.Header.Add("Authorization", "token "+GithubApiToken)
   resp, err := c.Do(req)
   if err != nil { return err, nil }
-  if resp.StatusCode != 200 { return err, nil }
+  if resp.StatusCode != 200 { return fmt.Errorf("failed to fetch file"), nil }
 
   var f githubFile
 
