79ac5c5dc9941152cd95bb3b53e0c77cca9ebb5f
Jonas Schneider <mail@jonasschneider.com>
2015-10-05 19:03:37 +0200

    fmt

--- build.go
+++ build.go
@@ -1,109 +1,137 @@
 package main
 
 import (
-  "net/http"
-  "io/ioutil"
-  "os"
-  "encoding/json"
-  "encoding/base64"
-  "strings"
-  "os/exec"
-  "io"
-  "log"
-  "time"
-  "fmt"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 type BuildResult struct {
-  Name string
-  Error error
-  DiagOut io.Reader
-  PrettyOut io.Reader
+	Name      string
+	Error     error
+	DiagOut   io.Reader
+	PrettyOut io.Reader
 }
 
 func fetchScript(github_repo_name, rev string) (error, io.Reader) {
-  fetchUrl := "https://api.github.com/repos/"+github_repo_name+"/contents/"+EntrypointPath+"?ref="+rev
-  var c http.Client
-  req, err := http.NewRequest("GET", fetchUrl, nil)
-  if err != nil { return err, nil }
-  req.Header.Add("Authorization", "token "+GithubApiToken)
-  resp, err := c.Do(req)
-  if err != nil { return err, nil }
-  if resp.StatusCode != 200 { return fmt.Errorf("failed to fetch build script %s",EntrypointPath), nil }
+	fetchUrl := "https://api.github.com/repos/" + github_repo_name + "/contents/" + EntrypointPath + "?ref=" + rev
+	var c http.Client
+	req, err := http.NewRequest("GET", fetchUrl, nil)
+	if err != nil {
+		return err, nil
+	}
+	req.Header.Add("Authorization", "token "+GithubApiToken)
+	resp, err := c.Do(req)
+	if err != nil {
+		return err, nil
+	}
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("failed to fetch build script %s", EntrypointPath), nil
+	}
 
-  var f githubFile
+	var f githubFile
 
-  data, err := ioutil.ReadAll(resp.Body)
-  if err != nil { return err, nil }
-  err = json.Unmarshal(data, &f)
-  if err != nil { return err, nil }
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err, nil
+	}
+	err = json.Unmarshal(data, &f)
+	if err != nil {
+		return err, nil
+	}
 
-  if f.Content == "" {
-    return fmt.Errorf("no build script %s found",EntrypointPath), nil
-  }
+	if f.Content == "" {
+		return fmt.Errorf("no build script %s found", EntrypointPath), nil
+	}
 
-  return nil, base64.NewDecoder(base64.StdEncoding, strings.NewReader(f.Content))
+	return nil, base64.NewDecoder(base64.StdEncoding, strings.NewReader(f.Content))
 }
 
 func runBuild(github_repo_name, rev string) BuildResult {
-  // fail immediately if duplicate
-  build_name := fmt.Sprintf("%d-%s", time.Now().Unix(), rev)
-  diagLogfile, err := os.Create(RootPath+"/log/"+build_name+".log")
-  if err != nil { return BuildResult{Error: err} }
+	// fail immediately if duplicate
+	build_name := fmt.Sprintf("%d-%s", time.Now().Unix(), rev)
+	diagLogfile, err := os.Create(RootPath + "/log/" + build_name + ".log")
+	if err != nil {
+		return BuildResult{Error: err}
+	}
 
-  err, scriptReader := fetchScript(github_repo_name, rev)
-  if err != nil { return BuildResult{Error: err} }
+	err, scriptReader := fetchScript(github_repo_name, rev)
+	if err != nil {
+		return BuildResult{Error: err}
+	}
 
-  cmd := exec.Command("bash")
-  cmd.Dir = "/tmp"
-  cmd.Stdin = scriptReader
-  cmd.Env = os.Environ()
-  cmd.Env = append(cmd.Env, "REPO=git@github.com:"+github_repo_name+".git")
-  cmd.Env = append(cmd.Env, "REFSPEC="+rev)
-  cmd.Env = append(cmd.Env, "CACHE="+RootPath+"/cache")
+	cmd := exec.Command("bash")
+	cmd.Dir = "/tmp"
+	cmd.Stdin = scriptReader
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "REPO=git@github.com:"+github_repo_name+".git")
+	cmd.Env = append(cmd.Env, "REFSPEC="+rev)
+	cmd.Env = append(cmd.Env, "CACHE="+RootPath+"/cache")
 
-  log.Println("Starting",EntrypointPath)
+	log.Println("Starting", EntrypointPath)
 
-  stdout, err := cmd.StdoutPipe()
-  if err != nil { return BuildResult{Error: err} }
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return BuildResult{Error: err}
+	}
 
-  stderr, err := cmd.StderrPipe()
-  if err != nil { return BuildResult{Error: err} }
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return BuildResult{Error: err}
+	}
 
-  output := io.MultiReader(stdout, stderr)
-  teedOut := io.TeeReader(output, os.Stderr)
+	output := io.MultiReader(stdout, stderr)
+	teedOut := io.TeeReader(output, os.Stderr)
 
-  err = cmd.Start()
-  if err != nil { return BuildResult{Error: err} }
+	err = cmd.Start()
+	if err != nil {
+		return BuildResult{Error: err}
+	}
 
-  err = os.MkdirAll(RootPath+"/log", 0777)
-  if err != nil { return BuildResult{Error: err} }
+	err = os.MkdirAll(RootPath+"/log", 0777)
+	if err != nil {
+		return BuildResult{Error: err}
+	}
 
-  diagLogfileRead, err := os.Open(diagLogfile.Name())
-  if err != nil { return BuildResult{Error: err} }
+	diagLogfileRead, err := os.Open(diagLogfile.Name())
+	if err != nil {
+		return BuildResult{Error: err}
+	}
 
-  _, err = io.Copy(diagLogfile, teedOut)
-  if err != nil { return BuildResult{Error: err, DiagOut: diagLogfileRead, Name: build_name} }
+	_, err = io.Copy(diagLogfile, teedOut)
+	if err != nil {
+		return BuildResult{Error: err, DiagOut: diagLogfileRead, Name: build_name}
+	}
 
-  err = cmd.Wait()
-  if err != nil { return BuildResult{Error: err, DiagOut: diagLogfileRead, Name: build_name} }
+	err = cmd.Wait()
+	if err != nil {
+		return BuildResult{Error: err, DiagOut: diagLogfileRead, Name: build_name}
+	}
 
-  return BuildResult{Error: nil, DiagOut: diagLogfileRead}
+	return BuildResult{Error: nil, DiagOut: diagLogfileRead}
 }
 
 func runAndReportBuild(repo_url, ref, rev, email string) {
-  github_repo_name := strings.Replace(repo_url, "https://github.com/", "", 1)
-  reportBuildStart(github_repo_name, ref, rev, email)
-  buildResult := runBuild(github_repo_name, rev)
-  if buildResult.Error != nil {
-    log.Println("error or failure while running build of",rev,":",buildResult.Error)
-    reportBuildFailure(github_repo_name, ref, rev, email, buildResult)
-  } else {
-    log.Println("build of",rev,"succeeded!")
-    reportBuildSuccess(github_repo_name, ref, rev, email, buildResult)
-  }
+	github_repo_name := strings.Replace(repo_url, "https://github.com/", "", 1)
+	reportBuildStart(github_repo_name, ref, rev, email)
+	buildResult := runBuild(github_repo_name, rev)
+	if buildResult.Error != nil {
+		log.Println("error or failure while running build of", rev, ":", buildResult.Error)
+		reportBuildFailure(github_repo_name, ref, rev, email, buildResult)
+	} else {
+		log.Println("build of", rev, "succeeded!")
+		reportBuildSuccess(github_repo_name, ref, rev, email, buildResult)
+	}
 }
 
 type githubFile struct {
-  Content string `json:"content"`
+	Content string `json:"content"`
 }
--- web.go
+++ web.go
@@ -1,70 +1,74 @@
 package main
 
 import (
-  "net/http"
-  "log"
-  "encoding/json"
-  "strings"
-  "fmt"
-  "os"
-  "io"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func handleGithubPost(w http.ResponseWriter, r *http.Request) {
-  r.ParseForm()
-  defer func() {
-    if r := recover(); r != nil {
-      log.Println(r)
-      w.WriteHeader(400)
-    }
-  }()
+	r.ParseForm()
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println(r)
+			w.WriteHeader(400)
+		}
+	}()
 
-  if r.Form.Get("secret") != GithubSharedSecret {
-    w.WriteHeader(401)
-    return
-  }
+	if r.Form.Get("secret") != GithubSharedSecret {
+		w.WriteHeader(401)
+		return
+	}
 
-  var f interface{}
-  err := json.Unmarshal([]byte(r.Form.Get("payload")), &f)
-  if err != nil { log.Println(err); w.WriteHeader(400); return; }
+	var f interface{}
+	err := json.Unmarshal([]byte(r.Form.Get("payload")), &f)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(400)
+		return
+	}
 
-  ref := f.(map[string]interface{})["ref"].(string)
-  rev := f.(map[string]interface{})["after"].(string)
+	ref := f.(map[string]interface{})["ref"].(string)
+	rev := f.(map[string]interface{})["after"].(string)
 
-  repo_obj := f.(map[string]interface{})["repository"].(map[string]interface{})
-  repo_url := repo_obj["url"].(string)
+	repo_obj := f.(map[string]interface{})["repository"].(map[string]interface{})
+	repo_url := repo_obj["url"].(string)
 
-  email := f.(map[string]interface{})["head_commit"].(map[string]interface{})["author"].(map[string]interface{})["email"].(string)
+	email := f.(map[string]interface{})["head_commit"].(map[string]interface{})["author"].(map[string]interface{})["email"].(string)
 
-  log.Println("Starting build of",repo_url,"ref",ref,"at",rev,"by",email)
-  go runAndReportBuild(repo_url, ref, rev, email)
+	log.Println("Starting build of", repo_url, "ref", ref, "at", rev, "by", email)
+	go runAndReportBuild(repo_url, ref, rev, email)
 
-  w.WriteHeader(200)
+	w.WriteHeader(200)
 }
 
 func handleBuildLog(w http.ResponseWriter, r *http.Request) {
-  r.ParseForm()
-  if r.Form.Get("secret") != GithubSharedSecret {
-    w.WriteHeader(401)
-    return
-  }
+	r.ParseForm()
+	if r.Form.Get("secret") != GithubSharedSecret {
+		w.WriteHeader(401)
+		return
+	}
 
-  parts := strings.Split(r.URL.Path, "/")
-  name := parts[len(parts)-1]
-  if strings.Index(name,"/") != -1 { // at least so *something*
-    w.WriteHeader(400)
-    return
-  }
-  path := fmt.Sprintf(RootPath+"/log/"+name+".log") // unsafe, but behind auth.. meeh
-  f, err := os.Open(path)
-  if err != nil {
-    log.Println(err)
-    w.WriteHeader(500)
-    return
-  }
-  w.Header().Add("Content-Type", "text/plain")
-  _, err = io.Copy(w, f)
-  if err != nil {
-    log.Println(err)
-  }
+	parts := strings.Split(r.URL.Path, "/")
+	name := parts[len(parts)-1]
+	if strings.Index(name, "/") != -1 { // at least so *something*
+		w.WriteHeader(400)
+		return
+	}
+	path := fmt.Sprintf(RootPath + "/log/" + name + ".log") // unsafe, but behind auth.. meeh
+	f, err := os.Open(path)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "text/plain")
+	_, err = io.Copy(w, f)
+	if err != nil {
+		log.Println(err)
+	}
 }
